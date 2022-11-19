699505c148b3a349e6af754e7b8c97ccb63f3a55
Dipra Das <dipradas5940@gmail.com>
2022-11-20 00:42:49 +0600

    73.7

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,25 @@
+import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const { signIn } = useContext(AuthContext);
+    const [loginError, setLoginError] = useState('');
+
     const handleLogin = data => {
-        console.log(data)
+        console.log(data);
+        setLoginError('');
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.log(error.message);
+                setLoginError(error.message);
+            })
     }
     return (
         <div className='h-[800px] flex justify-center items-center'>
@@ -22,13 +37,16 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" className="input input-bordered w-full max-w-xs" {...register("password", { required: "Password is required", minLength: {value: 6, message: 'Password must be 6 character longer'} })} />
+                        <input type="password" className="input input-bordered w-full max-w-xs" {...register("password", { required: "Password is required", minLength: { value: 6, message: 'Password must be 6 character longer' } })} />
                         {errors.password && <p className='text-red-600' role="alert">{errors.password?.message}</p>}
                         <label className="label">
                             <span className="label-text">Forger Password?</span>
                         </label>
                     </div>
                     <input className='btn btn-accent w-full' value="Login" type="submit" />
+                    <div>
+                        {loginError && <p className='text-red-600'>{loginError}</p>}
+                    </div>
                 </form>
                 <p>
                     New to Doctors Portal? <Link className='text-secondary' to="/signUp">Create a new account</Link>
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,18 +1,36 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+
     // SIGN UP
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    // Login
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user observing');
+            setUser(currentUser);
+        });
+        return () => unSubscribe();
+    }, [])
+
     const authInfo = {
-        createUser
+        createUser,
+        signIn,
+        user
     }
     return (
         <AuthContext.Provider value={authInfo}>
