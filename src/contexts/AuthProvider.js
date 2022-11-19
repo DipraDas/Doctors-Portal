30e11f0dd996a9f71388d0b75af7167dfa54104e
Dipra Das <dipradas5940@gmail.com>
2022-11-20 00:25:40 +0600

    73.6

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,15 +1,23 @@
 import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
-const AuthProvider = () => {
-    return (
-        <div>
+const AuthProvider = ({ children }) => {
+    // SIGN UP
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
-        </div>
+    const authInfo = {
+        createUser
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
