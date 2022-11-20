dabe6cf59d0f883902d4078541241290effbe0e8
Dipra Das <dipradas5940@gmail.com>
2022-11-20 11:20:44 +0600

    74.2

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -8,7 +8,7 @@ const AvailableAppointments = ({ selectedDate }) => {
     const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/appointmentOptions')
             .then(res => res.json())
             .then(data => setAppointmentOption(data))
     }, [])
