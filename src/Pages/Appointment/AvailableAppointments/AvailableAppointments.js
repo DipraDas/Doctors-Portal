82c6f6d82a53ca47433bfa4f9eb9ef0bd27ed6f6
Dipra Das <dipradas5940@gmail.com>
2022-11-20 13:29:10 +0600

    74.6

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,21 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
+import { useQuery } from '@tanstack/react-query';
 import React, {  useState } from 'react';
 import AppointmentOption from '../AppointmentOption/AppointmentOption';
 import BookingModal from '../BookingModal/BookingModal';
+import Loading from '../../Shared/Loading/Loading';
 
 const AvailableAppointments = ({ selectedDate }) => {
     // const [appointmentOption, setAppointmentOption] = useState([]);
     const [treatment, setTreatment] = useState(null);
+    const date = format(selectedDate, 'PP');
 
-    const { data: appointmentOptions = [] } = useQuery({
-        queryKey: ['appointmentOptions'],
+    const { data: appointmentOptions = [] , refetch, isLoading} = useQuery({
+        queryKey: ['appointmentOptions', date],
         queryFn: async () => {
-            const res = await fetch('http://localhost:5000/appointmentOptions')
+            const res = await fetch(`http://localhost:5000/appointmentOptions?date=${date}`)
             const data = await res.json();
             return data;
         }
     })
+
+    if(isLoading){
+        return <Loading></Loading>
+    }
 
     return (
         <div>
@@ -35,6 +41,7 @@ const AvailableAppointments = ({ selectedDate }) => {
                     treatment={treatment}
                     setTreatment={setTreatment}
                     selectedDate={selectedDate}
+                    refetch={refetch}
                 ></BookingModal>
             }
         </div>
