f486e0b8e8e1c9597a9aa1690a09a2b4f40580ab
Dipra Das <dipradas5940@gmail.com>
2022-11-21 13:06:11 +0600

    76.1

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -25,6 +25,7 @@ const DashboardLayout = () => {
                         {isAdmin &&
                             <>
                                 <li><Link to="/dashboard/allusers">Users</Link></li>
+                                <li><Link to="/dashboard/adddoctor">Add Doctor</Link></li>
                             </>
                         }
                     </ul>
---
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddDoctor = () => {
+    return (
+        <div>
+            <div className="text-3xl">Add Doctor</div>
+        </div>
+    );
+};
+
+export default AddDoctor;
